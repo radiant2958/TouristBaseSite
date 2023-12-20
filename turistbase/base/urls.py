4c813d62a4f1a590e55848e7052ea4833d69add7
Анна Шевченко <radiant5@MacBook-Air-Anna.local>
2023-12-20 19:35:11 +0700

    web-ver3

--- turistbase/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about-us', views.about, name='about-us'),
-    path('success/', views.success_page, name='success_page'),
-    path('contact/', views.contact, name='contact'),
-    path('blog/', views.new, name='new')
-]
